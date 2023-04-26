940752b8aea77c8f9370f2bc727ad567bb78dd13
Santiago Aloi <santiagoaloi@gmail.com>
2023-04-26 22:24:13 +0200

    chore(logo): update logo src

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="mt-n1 active-push-down-no-shadow flex">
-    <VImg class="mr-4" src="/logo.png" width="190" />
+    <VImg class="mr-4" src="/data_talks.png" width="190" />
   </div>
 </template>
